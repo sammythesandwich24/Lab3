a6f1995d4e47c76b5acfeca548c25dce64177cfa
sammythesandwich24 <stawade@purdue.edu>
2023-02-04 20:09:40 -0500

    Add project files.

---
+++ Lab3.cpp
@@ -0,0 +1,54 @@
+// Lab3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
+}
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value: ";
+	cin >> A;
+	cout << "Please enter the second value: ";
+	cin >> B;
+	cout << endl;
+}
+void firstChoice(float A, float B) {
+	cout << A << " + " << B << " = " << A + B << endl;
+}
+void secondChoice(float A, float B) {
+	cout << A << " - " << B << " = " << A - B << endl;
+}
+void thirdChoice(float A, float B) {
+	cout << A << " * " << B << " = " << A * B << endl;
+}
+void fourthChoice(float A, float B) {
+	cout << A << " / " << B << " = " << A / B << endl;
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		firstChoice(A, B);
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+	if (choice == 4) {
+		fourthChoice(A, B);
+	}
+	return 0;
+}
+
